dba23a140d30eeff5255eef3f25e91f06c5efd93
Wojciech Olejnik <divoolej@gmail.com>
2017-11-23 15:40:00 +0100

    Add database config

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :sejfguru, Sejfguru.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "sejfguru",
+  password: System.get_env("DATABASE_PASSWORD"),
   database: "sejfguru_dev",
   hostname: "localhost",
   pool_size: 10
