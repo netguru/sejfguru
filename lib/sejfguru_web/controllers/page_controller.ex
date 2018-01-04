f1534b16c7276352f9d52673b61094bee78ae522
Wojciech Olejnik <divoolej@gmail.com>
2018-01-04 16:05:45 +0100

    Expose current_user in the home page

--- lib/sejfguru_web/controllers/page_controller.ex
+++ lib/sejfguru_web/controllers/page_controller.ex
@@ -2,7 +2,9 @@ defmodule SejfguruWeb.PageController do
   use SejfguruWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    conn
+    |> assign(:current_user, Guardian.Plug.current_resource(conn))
+    |> render("index.html")
   end
 
   def protected(conn, _params) do
