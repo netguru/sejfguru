76f891b17769c0af0b2012ff1109e18fd2bc0939
Wojciech Olejnik <divoolej@gmail.com>
2018-01-04 14:50:47 +0100

    Remove unnecessary migrations

--- priv/repo/migrations/20180104133909_create_bookings.exs
+++ priv/repo/migrations/20180104133909_create_bookings.exs
@@ -2,8 +2,6 @@ defmodule Sejfguru.Repo.Migrations.CreateBookings do
   use Ecto.Migration
 
   def change do
-    drop table(:assets)
-
     create table(:bookings) do
       add :fs_device_id, :string
       add :user_id, references(:users)
