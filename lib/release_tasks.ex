0f5a2bacf530cfffd5652e10d46dc30917b80026
Radosław Piątek <radoslaw.piatek@netguru.pl>
2018-01-25 22:01:50 +0100

    Update migration command

--- lib/release_tasks.ex
+++ lib/release_tasks.ex
@@ -10,36 +10,67 @@ defmodule Sejfguru.ReleaseTasks do
     :ecto
   ]
 
-  @myapps [
-    :sejfguru
-  ]
+  def myapp, do: :sejfguru
 
-  @repos [
-    Sejfguru.Repo
-  ]
+  def repos, do: Application.get_env(myapp(), :ecto_repos, [])
 
-  def migrate do
-    IO.puts "Loading Sejfguru..."
-    :ok = Application.load(:sejfguru)
+  def seed do
+    # Run migrations
+    migrate()
 
-    IO.puts "Starting dependencies..."
-    Enum.each(@start_apps, &Application.ensure_all_started/1)
+    # Run seed script
+    Enum.each(repos(), &run_seeds_for/1)
 
-    IO.puts "Starting repos..."
-    Enum.each(@repos, &(&1.start_link(pool_size: 1)))
-
-    Enum.each(@myapps, &run_migrations_for/1)
-
+    # Signal shutdown
     IO.puts "Success!"
     :init.stop()
   end
 
-  defp priv_dir(app), do: "#{:code.priv_dir(app)}"
+  defp prepare do
+    me = myapp()
 
-  defp run_migrations_for(app) do
-    IO.puts "Running migrations for #{app}"
-    Ecto.Migrator.run(Sejfguru.Repo, migrations_path(app), :up, all: true)
+    IO.puts "Loading #{me}.."
+    # Load the code for myapp, but don't start it
+    :ok = Application.load(me)
+
+    IO.puts "Starting dependencies.."
+    # Start apps necessary for executing migrations
+    Enum.each(@start_apps, &Application.ensure_all_started/1)
+
+    # Start the Repo(s) for myapp
+    IO.puts "Starting repos.."
+    Enum.each(repos(), &(&1.start_link(pool_size: 1)))
   end
 
-  defp migrations_path(app), do: Path.join([priv_dir(app), "repo", "migrations"])
+  def migrate do
+    prepare()
+    Enum.each(repos(), &run_migrations_for/1)
+  end
+
+  def priv_dir(app), do: "#{:code.priv_dir(app)}"
+
+  defp run_migrations_for(repo) do
+    app = Keyword.get(repo.config, :otp_app)
+    IO.puts "Running migrations for #{app}"
+    Ecto.Migrator.run(repo, migrations_path(repo), :up, all: true)
+  end
+
+  def run_seeds_for(repo) do
+    # Run the seed script if it exists
+    seed_script = seeds_path(repo)
+    if File.exists?(seed_script) do
+      IO.puts "Running seed script.."
+      Code.eval_file(seed_script)
+    end
+  end
+
+  def migrations_path(repo), do: priv_path_for(repo, "migrations")
+
+  def seeds_path(repo), do: priv_path_for(repo, "seeds.exs")
+
+  def priv_path_for(repo, filename) do
+    app = Keyword.get(repo.config, :otp_app)
+    repo_underscore = repo |> Module.split |> List.last |> Macro.underscore
+    Path.join([priv_dir(app), repo_underscore, filename])
+  end
 end
