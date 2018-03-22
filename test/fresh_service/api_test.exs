f9d735af209c4ddb12860e397264591874974f46
Olga Draber <olga.draber@netguru.pl>
2018-03-22 22:02:58 +0100

    clean up freshservice api module and add tests

---
+++ test/fresh_service/api_test.exs
@@ -0,0 +1,68 @@
+defmodule FreshService.ApiTest do
+  use Sejfguru.DataCase
+  import FreshService.Api
+
+  defmodule Asset do
+    defstruct agent_id: nil
+  end
+
+  def json_response(map, status) do
+    {:ok, json} = Poison.encode(map)
+    %{body: json, status_code: status}
+  end
+
+  describe "get/2" do
+    test "returns parsed response (with struct) from given path" do
+      with_mocks([
+        {FreshService.Config, [], [
+          domain: fn() -> "sample" end,
+          username: fn() -> "user" end,
+          password: fn() -> "secret" end
+        ]},
+        {FreshService.Api, [:passthrough], [get!: fn(_url, _headers) -> json_response([%{agent_id: "id"}], 200) end]}
+      ]) do
+        assert {:ok, [%FreshService.ApiTest.Asset{agent_id: "id"}]} == get("/test.json", Asset)
+      end
+    end
+  end
+
+  describe "build_url/1" do
+    test "returns url build with domain from Config and path from argument" do
+      with_mock FreshService.Config, [domain: fn() -> "sample" end] do
+        path = "/test.json"
+        assert "https://sample.freshservice.com//test.json" == build_url(path)
+      end
+    end
+  end
+
+  describe "auth_header/2" do
+    test "returns headers if Authorization headers given" do
+      headers = ["Authorization": "Basic BASE64=="]
+      assert headers == auth_header(headers, %{})
+    end
+
+    test "returns headers if headers are not a list" do
+      headers = ["Authorization: Basic BASE64=="]
+      assert headers == auth_header(headers, %{})
+    end
+
+    test "returns encoded credential headers if credentials given" do
+      headers = []
+      credentials = {:username, :password}
+      assert [{:Authorization, "Basic dXNlcm5hbWU6cGFzc3dvcmQ="}] == auth_header(headers, credentials)
+    end
+
+    test "returns encoded credential headers if credentials given along with existing headers" do
+      headers = ["Content-Type": "application/json"]
+      credentials = {:username, :password}
+      assert ["Authorization": "Basic dXNlcm5hbWU6cGFzc3dvcmQ=", "Content-Type": "application/json"] == auth_header(headers, credentials)
+    end
+  end
+
+  describe "auth_header/1" do
+    test "returns headers if Authorization headers given" do
+      options = %{username: :username, password: :password}
+      assert ["Authorization": "Basic dXNlcm5hbWU6cGFzc3dvcmQ="] == auth_header(options)
+    end
+  end
+end
