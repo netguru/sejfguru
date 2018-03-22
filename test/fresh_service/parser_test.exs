fdc7ff86cbd9879f84003ec50ac03c3b0c61c95b
Olga Draber <olga.draber@netguru.pl>
2018-03-22 22:04:41 +0100

    better structure in freshservice parser test

--- test/fresh_service/parser_test.exs
+++ test/fresh_service/parser_test.exs
@@ -6,42 +6,45 @@ defmodule FreshService.ParserTest do
     defstruct agent_id: nil
   end
 
+  describe "parse/2" do
+    test "decodes a successful response into a struct" do
+      response = %{body: "{ \"agent_id\": \"some_id\" }", status_code: 200}
+      assert {:ok, %Asset{agent_id: "some_id"}} == parse(response, Asset)
+    end
 
-  test "parse/2 decodes a successful response into a struct" do
-    response = %{body: "{ \"agent_id\": \"some_id\" }", status_code: 200}
-    assert {:ok, %Asset{agent_id: "some_id"}} == parse(response, Asset)
+    test "returns an error for 404 response" do
+      response = %{body: "{ \"message\": \"Error message\" }", status_code: 400}
+      assert {:error, %{"message" => "Error message"}, 400} == parse(response, Asset)
+    end
   end
 
-  test "parse/2 returns an error for 404 response" do
-    response = %{body: "{ \"message\": \"Error message\" }", status_code: 400}
-    assert {:error, %{"message" => "Error message"}, 400} == parse(response, Asset)
-  end
+  describe "parse_list/2" do
+    test "decodes a successful response into a list of structs" do
+      json = """
+      [
+        {
+          "agent_id": "1"
+        },
+        {
+          "agent_id": "2"
+        }
+      ]
+      """
 
-  test "parse_list/2 decodes a successful response into a list of structs" do
-    json = """
-    [
-      {
-        "agent_id": "1"
-      },
-      {
-        "agent_id": "2"
-      }
-    ]
-    """
+      response = %{body: json, status_code: 200}
+      expected = [%Asset{agent_id: "1"}, %Asset{agent_id: "2"}]
 
-    response = %{body: json, status_code: 200}
-    expected = [%Asset{agent_id: "1"}, %Asset{agent_id: "2"}]
+      assert {:ok, expected} == parse_list(response, Asset)
+    end
 
-    assert {:ok, expected} == parse_list(response, Asset)
-  end
+    test "returns an error for 200 response that can't be decoded into struct" do
+      response = %{body: "{ \"access_denied\": \"true\" }", status_code: 200}
+      assert {:error, %{"access_denied" => "true"}, 200} == parse_list(response, Asset)
+    end
 
-  test "parse_list/2 returns an error for 200 response that can't be decoded into struct" do
-    response = %{body: "{ \"access_denied\": \"true\" }", status_code: 200}
-    assert {:error, %{"access_denied" => "true"}, 200} == parse_list(response, Asset)
-  end
-
-  test "parse_list/2 returns an error for 404 response" do
-    response = %{body: "{ \"message\": \"Error message\" }", status_code: 400}
-    assert {:error, %{"message" => "Error message"}, 400} == parse_list(response, Asset)
+    test "returns an error for 404 response" do
+      response = %{body: "{ \"message\": \"Error message\" }", status_code: 400}
+      assert {:error, %{"message" => "Error message"}, 400} == parse_list(response, Asset)
+    end
   end
 end
