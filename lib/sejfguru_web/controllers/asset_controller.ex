a3c2e376b56d52181f71854f3d48e113162362da
Radosław Piątek <radoslaw.piatek@netguru.pl>
2018-03-26 18:12:11 +0200

    Fix code review issues

--- lib/sejfguru_web/controllers/asset_controller.ex
+++ lib/sejfguru_web/controllers/asset_controller.ex
@@ -1,19 +1,15 @@
 defmodule SejfguruWeb.AssetController do
   use SejfguruWeb, :controller
 
-  def index(conn, %{"page" => page}) do
+  def index(conn, params) do
     conn
-    |> assign(:page, String.to_integer(page))
-    |> assign(:assets, fetch_assets(page))
-    |> render("index.html")
-  end
-
-  def index(conn, _params) do
-    conn
-    |> assign(:page, 1)
+    |> assign(:page, fetch_page(params))
     |> assign(:assets, fetch_assets(1))
     |> render("index.html")
   end
+
+  defp fetch_page(%{"page" => page}), do: String.to_integer(page)
+  defp fetch_page(_params), do: 1
 
   defp fetch_assets(page) do
     case FreshService.Asset.all(page) do
