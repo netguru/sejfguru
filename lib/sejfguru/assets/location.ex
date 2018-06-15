f3e69e6984fc6b2198b075879cf81681a94b7cb5
Kamil Paszkowski <kamil.paszkowski@netguru.pl>
2018-06-15 12:15:32 +0200

    location module, show city names in navbar

---
+++ lib/sejfguru/assets/location.ex
@@ -0,0 +1,26 @@
+defmodule Sejfguru.Assets.Location do
+  def city_lables do
+    Map.keys(locations().cities)
+  end
+
+  def other_labels do
+    Map.keys(locations().other)
+  end
+
+  def locations do
+    %{
+      cities: %{
+        "Białystok" => ["Białystok"],
+        "Gdańsk" => ["Grunwaldzka 103A"],
+        "Kraków" => ["Pawia 9", "Krakow Warehouse"],
+        "Poznań" => ["Wojskowa 6", "Poznan Warehouse"],
+        "Warszawa" => ["Jana Pawla II 29", "Warszawa Warehouse"],
+        "Wrocław" => ["Wita Stwosza 15/9A"],
+        "Zielona Góra" => ["Zielona Góra"],
+      },
+      other: %{
+        "Pozostałe" => ["Remote"],
+      }
+    }
+  end
+end
--- lib/sejfguru_web/controllers/asset_controller.ex
+++ lib/sejfguru_web/controllers/asset_controller.ex
@@ -1,10 +1,13 @@
 defmodule SejfguruWeb.AssetController do
   use SejfguruWeb, :controller
   require Ecto.Query
+  alias Sejfguru.Assets.Location
 
   def index(conn, params) do
     conn
     |> assign(:page, fetch_page(params))
+    |> assign(:city_locations, Location.city_lables())
+    |> assign(:other_locations, Location.other_labels())
     |> assign(:assets, fetch_assets(fetch_page(params)))
     |> render("index.html")
   end
