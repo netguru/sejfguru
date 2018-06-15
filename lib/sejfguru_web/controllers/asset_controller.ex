17f8d845be62fc4f4ff9e4acefcce0d57d9cd610
Kamil Paszkowski <kamil.paszkowski@netguru.pl>
2018-06-15 12:30:37 +0200

    show current location

--- lib/sejfguru_web/controllers/asset_controller.ex
+++ lib/sejfguru_web/controllers/asset_controller.ex
@@ -8,12 +8,17 @@ defmodule SejfguruWeb.AssetController do
     |> assign(:page, fetch_page(params))
     |> assign(:city_locations, Location.city_lables())
     |> assign(:other_locations, Location.other_labels())
+    |> assign(:current_location, fetch_location(params))
     |> assign(:assets, fetch_assets(fetch_page(params)))
     |> render("index.html")
   end
 
   defp fetch_page(%{"page" => page}), do: String.to_integer(page)
   defp fetch_page(_params), do: 1
+
+  # IDEA: Maybe we should geolocate user, and select the nearest city as default option
+  defp fetch_location(%{"location" => location}), do: location
+  defp fetch_location(_params), do: List.first(Location.city_lables())
 
   defp fetch_assets(page) do
     Sejfguru.Assets.list_assets(type: "Mobile", page: page)
